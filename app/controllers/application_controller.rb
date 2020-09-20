ff958a0444e9a289afa9cc0004e47b54ec382d13
Lanea <lanearohan@gmail.com>
2020-09-20 18:42:48 +0000

    Working login/sign up/sign out pages - all redirect to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
     
-    def sign_in
+    def sign_in(email, password)
+       # redirect_to "/home"
     end
+    
+    def sign_up(email, password, password_confirm)
+       # redirect_to "/home"
+    end 
+    
     
     def home
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'user/sessions'
+    #registrations: 'user/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
-  
-  unauthenticated do 
-    as :user do
-      root :to => "applications#sign_up"
-    end
-  end 
   
   root :to => "application#home"
   
