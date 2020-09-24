798911b3fe7030a3b6efd84acd150d1ae56e728f
Lanea <lanearohan@gmail.com>
2020-09-24 22:10:08 +0000

    fixed pull request comments - pull request #8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,6 @@
 class ApplicationController < ActionController::Base
-    
-    def sign_in(email, password)
-       # redirect_to "/home"
-    end
-    
-    def sign_up(email, password, password_confirm)
-       # redirect_to "/home"
-    end 
-    
-    
+
     def home
     end
-    
-    def layout_by_resource 
-        if user_signed_in?
-            "home"
-        else 
-            "sign_in"
-        end 
-    end 
     
 end
