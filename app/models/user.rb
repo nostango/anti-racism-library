848afb0020ce2e1f5f53f0db7e416e6566cb67dd
cvrkota <cvrkotam@grinnell.edu>
2020-10-07 23:47:18 +0000

    accept only grinnell email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  VALID_GRINNELL_EMAIL_REGEX = /\A[\w+\-.]+@grinnell\.edu/i
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, format: { with: VALID_GRINNELL_EMAIL_REGEX }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
