1af3eda8f078db7730fe488d32d386f4e5a1577b
cvrkota <cvrkotam@grinnell.edu>
2020-10-18 22:39:40 +0000

    fix email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ApplicationRecord
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
+  attr_writer :login
   VALID_GRINNELL_EMAIL_REGEX = /\A[\w+\-.]+@grinnell\.edu/i
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :email, format: { with: VALID_GRINNELL_EMAIL_REGEX }
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  validates_format_of :email, :with => VALID_GRINNELL_EMAIL_REGEX , :message => "domain must be 'grinnell.edu'"
+  
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
         
 end
