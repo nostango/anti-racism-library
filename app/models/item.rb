58173fd74d39d116945464d1b95f484103a498fe
Lanea Rohan <43596091+athaichi@users.noreply.github.com>
2020-10-20 10:05:51 -0500

    Update item.rb
    fix url to be optional

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,5 @@ class Item < ApplicationRecord
     validates :title, presence: true, length: { maximum: 1000 }
     validates :author, presence: true, length: { maximum: 1000 }
     validates :description, presence: true, length: { maximum: 1000 }
-    validates :url, presence: true
     validates :category, presence: true
 end
